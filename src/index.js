4ee63dbe3013714032d718ecf26d73162061ba15
astubbings <53932400+astubbings@users.noreply.github.com>
2019-11-11 14:32:16 -0700

    first changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
